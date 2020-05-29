ad183f9c248cf483bc6cd617b89b8572dd0beb62
surendra172001 <52756184+surendra172001@users.noreply.github.com>
2020-05-29 15:02:37 +0530

    date : 29/05/20

---
+++ strange.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pi_(x)	printf("%d ",x)
+#define pl(x)	printf("%lld\n",x)
+#define pl_(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define nl	    printf("\n")
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+const int MAX = (1e5)+5;
+
+int a[MAX];
+int b[MAX];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t;
+    si(t);
+    while(t--) {
+      int i, n, k;
+      si(n); si(k);
+      fo(i, n) si(a[i]);
+      fo(i, n) si(b[i]);
+      int maxB = INT_MIN;
+      fo(i, n) maxB = max(maxB, b[i]);
+      maxB += 1;
+      ll cnt = 0;
+      fo(i, n) cnt += (maxB > a[i] ? maxB - a[i] : 0);
+      // pi(cnt);
+      ll ans = cnt * k;
+      pl(ans);
+    }
+
+    return 0;
+}
